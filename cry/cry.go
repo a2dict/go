57567a8fa9cde8a88492af85f78e6355752bbdf2
a2dict <a2dict@163.com>
2020-02-21 12:02:43 +0800

    add rsa util

--- cry/cry.go
+++ cry/cry.go
@@ -6,6 +6,9 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/base64"
+	"encoding/pem"
+
+	"github.com/a2dict/go/str"
 )
 
 // ParseBase64PKCS8PrivateKey ...
@@ -19,6 +22,19 @@ func ParseBase64PKCS8PrivateKey(s string) (priv *rsa.PrivateKey, err error) {
 		return
 	}
 	priv = k.(*rsa.PrivateKey)
+	return
+}
+
+// ParseBase64PKCS1PrivateKey ...
+func ParseBase64PKCS1PrivateKey(s string) (priv *rsa.PrivateKey, err error) {
+	bs, err := base64.StdEncoding.DecodeString(s)
+	if err != nil {
+		return
+	}
+	priv, err = x509.ParsePKCS1PrivateKey(bs)
+	if err != nil {
+		return
+	}
 	return
 }
 
@@ -49,8 +65,8 @@ func RsaSignWithHash(content string, hash crypto.Hash, priv *rsa.PrivateKey) (st
 	return s, nil
 }
 
-// RsaSignVerifyWithHash ...
-func RsaSignVerifyWithHash(content, sign string, hash crypto.Hash, pub *rsa.PublicKey) error {
+// VerifyRsaSignWithHash ...
+func VerifyRsaSignWithHash(content, sign string, hash crypto.Hash, pub *rsa.PublicKey) error {
 	h := hash.New()
 	h.Write([]byte(content))
 	digest := h.Sum(nil)
@@ -59,4 +75,86 @@ func RsaSignVerifyWithHash(content, sign string, hash crypto.Hash, pub *rsa.Publ
 		return err
 	}
 	return rsa.VerifyPKCS1v15(pub, hash, digest, signature)
+}
+
+// ParsePemPKCS8PrivateKey ...
+func ParsePemPKCS8PrivateKey(s string) (priv *rsa.PrivateKey, err error) {
+	block, _ := pem.Decode([]byte(s))
+	k, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	priv = k.(*rsa.PrivateKey)
+	return
+}
+
+// ParsePemPKCS1PrivateKey ...
+func ParsePemPKCS1PrivateKey(s string) (priv *rsa.PrivateKey, err error) {
+	block, _ := pem.Decode([]byte(s))
+	priv, err = x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	return
+}
+
+// ParsePemPKIXPublicKey ...
+func ParsePemPKIXPublicKey(s string) (pub *rsa.PublicKey, err error) {
+	block, _ := pem.Decode([]byte(s))
+	k, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return
+	}
+	pub = k.(*rsa.PublicKey)
+	return
+}
+
+// RsaKeyPair ...
+type RsaKeyPair struct {
+	Priv         *rsa.PrivateKey
+	Pub          *rsa.PublicKey
+	PrivPKCS1Pem string
+	PubPKIXPem   string
+}
+
+// GenRsaKey ...
+func GenRsaKey(bits int) (*RsaKeyPair, error) {
+	priv, err := rsa.GenerateKey(rand.Reader, bits)
+	if err != nil {
+		return nil, err
+	}
+	var privBlock = &pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(priv),
+	}
+	privPkcs1PemBytes := pem.EncodeToMemory(privBlock)
+
+	pub := &priv.PublicKey
+	pubBlockBytes, err := x509.MarshalPKIXPublicKey(pub)
+
+	pubBlock := &pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: pubBlockBytes,
+	}
+	pubPkixPemBytes := pem.EncodeToMemory(pubBlock)
+	return &RsaKeyPair{
+		Priv:         priv,
+		Pub:          pub,
+		PrivPKCS1Pem: string(privPkcs1PemBytes),
+		PubPKIXPem:   string(pubPkixPemBytes),
+	}, nil
+}
+
+// VerifyKeyPair ...
+func VerifyKeyPair(priv *rsa.PrivateKey, pub *rsa.PublicKey) error {
+	content := str.RandStrWithCharset(32, str.Letters)
+	s, err := RsaSignWithHash(content, crypto.SHA256, priv)
+	if err != nil {
+		return err
+	}
+	err = VerifyRsaSignWithHash(content, s, crypto.SHA256, pub)
+	if err != nil {
+		return err
+	}
+	return nil
 }
